bc502584961382fd8f9b66f1da33505b792f3e89
Leonardo Kawamoto <leonardohk10@hotmail.com>
2026-08-23 15:49:09 -0300

    chore: remove unused code

--- backend/src/infra/database/prisma.mock.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client/extension';
-import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
-
-export type PrismaMock = DeepMockProxy<PrismaClient>;
-
-export const createPrismaMock = () => mockDeep<PrismaClient>();
